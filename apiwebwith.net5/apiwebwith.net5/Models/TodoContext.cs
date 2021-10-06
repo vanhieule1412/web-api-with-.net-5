643f7725ee541d68dd3a80762020153928dee72e
Harris Le <hieu.levan1412@gmail.com>
2021-10-06 11:17:58 +0700

    first commit

---
+++ apiwebwith.net5/apiwebwith.net5/Models/TodoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiwebwith.net5.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+
+        }
+        public List<TodoItem> TodoItemlist = new List<TodoItem>()
+        {
+            new TodoItem{Id =1, Name="H", IsComplete=true, Secret= "asdxc"},
+            new TodoItem{Id =2, Name="HK", IsComplete=true, Secret= "zxcv"},
+        };
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
